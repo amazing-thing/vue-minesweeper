4001e3c44bcad210b304460546c6fd753511d29f
神奇的是 <1821238705@qq.com>
2022-03-26 22:55:07 +0800

    chore：更新

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
 const HEIGHT = 10
 const WIDTH = 10
 
-interface blockState {
+interface BlockState {
   x: number
   y: number
   reversed: boolean// 是否翻转
@@ -35,49 +35,102 @@ const numberClass = [
 ]
 
 // 数字颜色
-function getNumberClass(block: blockState) {
-  if (!block.reversed)
+function getNumberClass(block: BlockState) {
+  if (block.flagged)
     return 'bg-gray-500/10'
+
+  if (!block.reversed)
+    return 'bg-gray-500/10 hover:bg-gray/20'
 
   return numberClass[block.adjanceMine]
 }
 
 const state = reactive(Array.from({ length: HEIGHT }, (_, y) => (
-  Array.from({ length: WIDTH }, (_, x): blockState => ({
+  Array.from({ length: WIDTH }, (_, x): BlockState => ({
     x, y, adjanceMine: 0, reversed: false,
   }))),
 ))
 
+// 右键
+function onRightClick(block: BlockState) {
+  if (block.reversed)
+    return
+  block.flagged = !block.flagged
+}
+
 // 生成炸弹
-function createMine() {
+function createMine(initialize: BlockState) {
   for (const row of state) {
-    for (const block of row)
+    for (const block of row) {
+      if (Math.abs(initialize.x - block.x) <= 1)
+        continue
+      if (Math.abs(initialize.y - block.y) <= 1)
+        continue
+
       block.mine = Math.random() < 0.2
+    }
   }
+
+  createNumber()
 }
 
 // 生成数字
 function createNumber() {
-  for (const row of state) {
-    for (const block of row) {
-      position.forEach((p) => {
-        const x2 = p[0] + block.x
-        const y2 = p[1] + block.y
-        if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
-          return
-        if (state[y2][x2].mine)
-          block.adjanceMine += 1
+  state.forEach((row) => {
+    row.forEach((block) => {
+      if (block.mine)
+        return
+      generator(block).forEach((b) => {
+        if (b.mine)
+          block.adjanceMine++
       })
+    })
+  })
+}
+
+// 重用
+function generator(block: BlockState) {
+  return position.map(([dx, dy]) => {
+    const x2 = dx + block.x
+    const y2 = dy + block.y
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return undefined
+
+    return state[y2][x2]
+  }).filter(Boolean) as BlockState[]
+}
+
+// 展开0
+function expendeZero(initialize: BlockState) {
+  if (initialize.adjanceMine)
+    return
+
+  generator(initialize).forEach((b) => {
+    if (!b.reversed) {
+      b.reversed = true
+      expendeZero(b)
     }
+  })
+}
+
+let mineGenerator = false
+
+// 点击
+function onClick(block: BlockState) {
+  if (!mineGenerator) {
+    createMine(block)
+    mineGenerator = true
   }
-}
 
-function onClick(block: blockState) {
+  if (block.flagged)
+    return
+
+  if (block.mine)
+    alert('you lose')
+
   block.reversed = true
+  expendeZero(block)
 }
-
-createMine()
-createNumber()
 
 </script>
 
@@ -93,12 +146,15 @@ createNumber()
         flex="~" items-center justify-center
         w10 h10
         border="1 gray-400/10"
-        m=".5"
-        hover="bg-gray/10"
+        m=".2"
         :class="getNumberClass(item)"
         @click="onClick(item)"
+        @contextmenu.prevent="onRightClick(item)"
       >
-        <template v-if="item.reversed">
+        <template v-if="item.flagged">
+          <div i-mdi-flag text-red />
+        </template>
+        <template v-else-if="item.reversed">
           <div v-if="item.mine" class="i-mdi-mine" bg-red-500 />
           <div v-else>
             {{ item.adjanceMine }}
